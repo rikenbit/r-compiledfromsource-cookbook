0f6492a9c21aa6ee0a2bb89f9cfed531bd816c20
Manabu ISHII <manabu.ishii.rb@gmail.com>
2015-11-20 13:27:27 +0900

    Change apt source only if OS is Ubuntu

--- recipes/change-apt.rb
+++ recipes/change-apt.rb
@@ -1,6 +1,9 @@
 # update /etc/apt/sources.list at compile time
-e = execute "sed -i 's/us.archive.ubuntu.com/ftp.jaist.ac.jp/' /etc/apt/sources.list ; apt-get update" do
-  action :nothing
-end
+case node.platform
+when "ubuntu"
+  e = execute "sed -i 's/us.archive.ubuntu.com/ftp.jaist.ac.jp/' /etc/apt/sources.list ; apt-get update" do
+    action :nothing
+  end
 
-e.run_action(:run)
+  e.run_action(:run)
+end
