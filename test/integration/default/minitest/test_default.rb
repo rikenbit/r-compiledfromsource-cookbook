f91798e94f06db24fec75dbc2e10eee06f40fe15
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-10-02 16:22:09 +0900

    Update R version for default

--- test/integration/default/minitest/test_default.rb
+++ test/integration/default/minitest/test_default.rb
@@ -7,6 +7,6 @@ describe 'check R version' do
   it "check R version" do
     system('echo "q()" > /tmp/showversion.R')
     system('/usr/local/R-devel/bin/R CMD BATCH /tmp/showversion.R')
-    assert system('grep "R version 3.4.2 RC" showversion.Rout'), 'R version is not expected version. patched version is updated'
+    assert system('grep "R version 3.4.2 Patched" showversion.Rout'), 'R version is not expected version. patched version is updated'
   end
 end
